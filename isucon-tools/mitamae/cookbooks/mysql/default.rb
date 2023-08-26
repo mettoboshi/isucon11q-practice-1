80fa8c43893e6802993298ec3ed3d3eb3b9199a6
mettoboshi <mettoboshi.1108@gmail.com>
2023-08-26 12:19:44 +0900

    add mysql-conf

--- isucon-tools/mitamae/cookbooks/mysql/default.rb
+++ isucon-tools/mitamae/cookbooks/mysql/default.rb
@@ -3,15 +3,16 @@ current_time = Time.now.to_s.gsub(/[- :+]/, '')
 
 # カレントディレクトリを取得
 current_dir = File.dirname(__FILE__)
-source_path = File.expand_path("config/mysql.conf.d/mysqld.cnf", current_dir)
+# source_path = File.expand_path("config/mysql.conf.d/mysqld.cnf", current_dir)
+source_path = File.expand_path("config/mariadb.conf.d/50-server.cnf", current_dir)
 
 # mysqld.cnfのバックアップ
 execute "backup mysqld.conf" do
-  command "mv /etc/mysql/mysql.conf.d/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf.#{current_time}"
-  not_if "diff -q /etc/mysql/mysql.conf.d/mysqld.cnf #{source_path}"
+  command "mv /etc/mysql/mariadb.conf.d/50-server.cnf /etc/mysql/mariadb.conf.d/50-server.cnf.#{current_time}"
+  not_if "diff -q /etc/mysql/mariadb.conf.d/50-server.cnf #{source_path}"
 end
 
-remote_file "/etc/mysql/mysql.conf.d/mysqld.cnf" do
+remote_file "/etc/mysql/mariadb.conf.d/50-server.cnf" do
   owner  "root"
   group  "root"
   source source_path
